40a6f0662a539fa4e72d2f9fa47983a39a00278f
prasad.gs <prasad.gs@SON16195>
2017-05-31 17:38:57 +0530

    Basci Skeleton

---
+++ StudentMarg.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentMarg.Web.MvcApplication" Language="C#" %>
---
+++ StudentMarg.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentMarg.Web.Startup))]
+namespace StudentMarg.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
